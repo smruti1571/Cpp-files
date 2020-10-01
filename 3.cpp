2aa581941153fa0ab48a7c0b10515c04eafdd1e0
AnkanSwain <56729710+AnkanSwain@users.noreply.github.com>
2020-10-01 14:53:45 +0530

    Update 3.cpp
    Just made it easier to understand

--- 3.cpp
+++ 3.cpp
@@ -1,11 +1,15 @@
+//Comparing four numbers to find the maximum among them
 #include<stdio.h>
 int main()
 {
 	int a,b,c,d,max1,max2;
 	printf("Enter the numbers:");
 	scanf("%d %d %d %d",&a,&b,&c,&d);
+	//comparing a & b
 	max1=a>=b?a:b;
+	//comparing c & d
 	max2=c>=d?c:d;
+	//comparing the max values between (a and b) & (c and d)
 	max1>=max2?printf("Maximum=%d",max1):printf("Maximum:%d",max2);
     return 0;
 }
